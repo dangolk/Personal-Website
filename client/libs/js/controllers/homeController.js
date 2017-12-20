13885b5b28a2e22fe20c0ec64f7893c48062d194
Kiran Dangol <kiran_dan2009@yahoo.com>
2017-12-20 00:28:20 +0000

    push to github from c9

---
+++ client/libs/js/controllers/homeController.js
@@ -0,0 +1,11 @@
+myApp.controller('homeController', ['$scope', function($scope){
+    $scope.slides = [{
+        image: "img/nepali.png"
+    },
+    {
+        image: "img/basketball.png"
+    },
+    {
+        image: "img/entrepreneurial.png"
+    }]
+}]);
